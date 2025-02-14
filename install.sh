af82e668b57409561e757be43b0b8d50a02678aa
commonlainse <femboygordonfreeman+lainse@gmail.com>
2025-02-14 06:48:48 -0300

    + lmms

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ flush () {
 }
 
 # Packages go here
-pkg stow git emacs torbrowser-launcher okular # General purpose
+pkg stow git emacs torbrowser-launcher okular lmms # General purpose
 pkg autoconf texinfo libgtk-3-dev libgif-dev libxpm-dev libgnutls28-dev libncurses-dev libmagickwand-dev libtree-sitter-dev # for compiling Emacs
 flush
 
