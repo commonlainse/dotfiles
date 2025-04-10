3c33488288f331e783ceeea49501eefceb485cfd
commonlainse <femboygordonfreeman+lainse@gmail.com>
2025-04-10 16:28:20 -0300

    +vscode

--- install.sh
+++ install.sh
@@ -85,6 +85,15 @@ if ! [ -f /usr/share/keyrings/waydroid.gpg ]; then
 fi
 pkg waydroid weston
 
+# Visual Studio Code (sigh)
+if ! [ -f /etc/apt/keyrings/microsoft-archive-keyring.gpg ]; then
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/keyrings/microsoft-archive-keyring.gpg
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt-get update
+fi
+pkg code
+
 flush
 
 # Emacs config
